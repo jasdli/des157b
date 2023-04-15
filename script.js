70f84b33345a2088e48c92dcef31c67875d1b954
Jasmine Li <jasmineli1215@gmail.com>
2023-04-15 00:25:50 -0700

    home

--- script.js
+++ script.js
@@ -4,7 +4,13 @@
     const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
+    const sections = document.querySelectorAll('section');
+    const buttonIcon = document.querySelector('button img');
+  
+
+   const officeBanner = document.querySelector('#office-animation');
+    const nightBanner = document.querySelector('#night-animation');
+
     let mode = 'dark';
 
     button.addEventListener('click', function() {
@@ -12,6 +18,9 @@
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
+            buttonIcon.src = 'images/button1.png';
+            officeBanner.className = 'hidden';
+            nightBanner.className = 'showing'
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -20,6 +29,9 @@
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
+            buttonIcon.src = 'images/button2.png';
+            officeBanner.className = 'showing';
+            nightBanner.className = 'hidden'
             for (const section of sections) {
                 section.removeAttribute('class');
             }
