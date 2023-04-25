62512606a514bf76baf49d3cf56a19dffb9e7d9c
Jasmine Li <jasmineli1215@gmail.com>
2023-04-25 03:05:14 -0700

    blockbuster

--- studio1/script.js
+++ studio1/script.js
@@ -1,37 +1,25 @@
 (function(){
     'user strict';
     console.log('reading js');
-    const conVid = document.querySelector('#concertVid');
-    const playToggle = document. querySelector('.fa-play');
-        const volToggle = document.querySelector('.fa-volume');
-        const volLevel = document.querySelector('#volumeLevel');
-        let playing = false;
-        playToggle.addEventListener('click', function(){
-            if (!playing){
-                myVideo.play();
-                playToggle.className = 'fa-solid fa-pause';
-            } else{
-                myVideo.pause();
-                playToggle.className = 'fa-solid fa-play';
 
-            }
-            plating = !playing;
-        })
-        volToggle.addEventListener('click', function(){
-            if (volToggle.className === 'fas fa-volume-up'){
-                volToggle.className = 'fas fa-volume-mute';
-                myVideo.muted = true;
-            } else {
-                volToggle.className = 'fas fa-volume-up';
-                myVideo.muted = false;
-            }
-        })
-        volLevel.addEventListener('change', function(){
-            changeVolume(volLevel.value);
-        })
-        function changeVolume(value){
-            myVideo.volume = value/100;
-            console.log('volume is ' + myVideo.volume);
-        }
+ const video = document.querySelector('video');
+ const playButton = document.querySelector('.play-button');
+
+playButton.addEventListener('click', function() {
+  if (video.paused) {
+    video.play();
+    playButton.textContent = 'Pause';
+  } else {
+    video.pause();
+    playButton.textContent = 'Play';
+  }
+});
+
+
+
 
         })();
+
+          
+          
+          
