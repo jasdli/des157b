3ebbf9b9b46c78687c8d16ee73ecd5a6b00b41cf
Jasmine Li <jasmineli1215@gmail.com>
2023-05-09 15:02:27 -0700

    1

--- friendslist/script.js
+++ friendslist/script.js
@@ -5,15 +5,48 @@ const newBtn = document.getElementById("newbtn");
 const editBtns = document.querySelectorAll(".fa-edit");
 const addFriendForm = document.getElementById("add-friend");
 const editFriendForm = document.getElementById("edit-friend");
+const friendsList = document.querySelector("main ol");
 
 async function displayFriends() {
     const friends = Parse.Object.extend('Friends');
     const query = new Parse.Query(friends);
     const results = await query.ascending('lname').find();
-    console.log(results);
-}
+   // console.log(results);
+   results.forEach(function(eachFriend){
+    const id = eachFriend.id;
+    const lname = eachFriend.get('lname');
+    const fname = eachFriend.get('fname');
+    const email = eachFriend.get('email');
+    const facebook = eachFriend.get('facebook');
+    const twitter = eachFriend.get('twitter');
+    const instagram = eachFriend.get('instagram');
+    const linkedin = eachFriend.get('linkedin');
 
+    const theListItem = document.createElement("li");
+    theListItem.setAttribute("id", `r-${id}`);
+    theListItem.innerHTML = `template here...`;
+   });
+
+   friendsList.append(theListItem);
+
+   theListItem.innerHTML = `<div class="name">
+   ${fname} ${lname}</div>
+<div class="email">
+   <i class="fas fa-envelope-square"></i>  ${email}
+</div>
+<div class="social">
+   <a href=" ${facebook}"><i class="fab fa-facebook-square"></i></a>
+   <a href=" ${twitter}"><i class="fab fa-twitter-square"></i></a>
+   <a href=" ${instagram}"><i class="fab fa-instagram"></i></a>
+   <a href=" ${linkedin}"><i class="fab fa-linkedin"></i></a>
+</div>
+<i class="fas fa-edit" id="e-${id}"></i>
+<i class="fas fa-times-circle" id="d-${id}"></i>`;
+}
 displayFriends();
+
+
+
 
 newBtn.addEventListener("click", function(event){
     event.preventDefault();
