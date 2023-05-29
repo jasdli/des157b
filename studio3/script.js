e8832e34fdba26990126e3febbe3f2538c755240
Jasmine Li <jasmineli1215@gmail.com>
2023-05-28 19:51:31 -0700

    1

--- studio3/script.js
+++ studio3/script.js
@@ -1,68 +1,49 @@
-const map = L.map('map').setView([38.546719, -121.744339], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-/*const fullMap = new L.Map('map', {
-    fullscreenControl: true,
-    // OR
-    fullscreenControl: {
-        pseudoFullscreen: false // if true, fullscreen to page width and height
-    }
-});*/
-const marker = L.marker([38.543487, -121.753717]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-const popup = L.popup()
-    .setLatLng([38.5617427,-121.7638948])
-    .setContent("where i live")
-    .openOn(map);
-
+(function () {
+    'use strict';
   
-
-    function onMapClick(e) {
-        popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
-    }
-    
-    map.on('click', onMapClick);
-
+    const map = L.map('map').setView([38.546719, -121.744339], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+  
     const coffeeIcon = L.icon({
-        iconUrl: 'images/coffee.png',
-    
-        iconSize:     [38, 95], // size of the icon
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      iconUrl: 'images/coffee.png',
+      iconSize: [48, 35],
+      iconAnchor: [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor: [-3, -76]
     });
-    L.marker([	38.5639426043, -121.472421579], {icon: coffeeIcon}).addTo(map).bindPopup("I am a green leaf.");
+    L.marker([38.5639426043, -121.472421579], { icon: coffeeIcon }).addTo(map);
+  
+    const schoolIcon = L.icon({
+      iconUrl: 'images/school.png',
+      iconSize: [48, 35],
+      iconAnchor: [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor: [-3, -76]
+    });
 
-   // different restaurants
-    /*const littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
-    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
-    aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
-    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');*/
-   //vegan restaurants 
-    /*const vegan = L.layerGroup([littleton, denver, aurora, golden]);*/
+    L.marker([38.5450052, -121.7537113], { icon: schoolIcon }).addTo(map);
 
-/*// or, add to an existing map:
-fullMap.addControl(new L.Control.Fullscreen());
-
-fullMap.isFullscreen() // Is the map fullscreen?
-fullMap.toggleFullscreen() // Either go fullscreen, or cancel the existing fullscreen.
-
-// `fullscreenchange` Event that's fired when entering or exiting fullscreen.
-fullMap.on('fullscreenchange', function () {
-    if (fullMap.isFullscreen()) {
-        console.log('entered fullscreen');
-    } else {
-        console.log('exited fullscreen');
-    }
-});
-fullMap.addControl(new L.Control.Fullscreen({
-    title: {
-        'false': 'View Fullscreen',
-        'true': 'Exit Fullscreen'
-    }
-}));*/
+    const apartmentIcon = L.icon({
+      iconUrl: 'images/apartment.png',
+      iconSize: [30, 45],
+      iconAnchor: [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor: [-3, -76]
+    });
+    L.marker([38.5620644, -121.7632155], { icon: apartmentIcon }).addTo(map);
+    
+  
+    const appleIcon = L.icon({
+      iconUrl: 'images/apple.png',
+      iconSize: [45, 45],
+      iconAnchor: [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor: [-3, -76]
+    });
+    L.marker([38.5467537, -121.7614973], { icon: appleIcon }).addTo(map);
+  })();
+  
+  
--- version1/script.js
+++ version1/script.js
@@ -4,3 +4,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+const marker = L.marker([38.5414, -121.7410]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>My Burma").openPopup();
+
+const popup = L.popup()
+    .setLatLng([38.544123,-121.7391475])
+    .setContent("Temple Coffee Roaster at G street")
+    .openOn(map);
+
+    const circle = L.circle([38.54475754871805,-121.73894408741701], {
+        color: 'greem',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 20
+    }).addTo(map);
+    circle.bindPopup("Woodstock Piza Davis");
+
