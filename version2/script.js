17f2ce80d270296f386954539af1f33b34883a27
Jasmine Li <jasmineli1215@gmail.com>
2023-05-30 13:40:15 -0700

    1

--- version2/script.js
+++ version2/script.js
@@ -1,27 +1,3 @@
-//map
-/*const map = L.map('map').setView([38.546719, -121.744339], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-const marker = L.marker([38.5414, -121.7410]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>My Burma").openPopup();
-
-const popup = L.popup()
-    .setLatLng([38.544123,-121.7391475])
-    .setContent("Temple Coffee Roaster at G street")
-    .openOn(map);
-
-    const circle = L.circle([38.54475754871805,-121.73894408741701], {
-        color: 'greem',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 20
-    }).addTo(map);
-    circle.bindPopup("Woodstock Piza Davis");*/
-//upload photos
-//app id and js key from B4A
 Parse.initialize("PUqIfC6upxBMI8ktgDLqsT0r5mAF3ma1q1bdaKkb", "FlP4kl3OqVBnEJ0yteYIH4BFCyPKnYhcRhEdQlJx");
 
 Parse.serverURL = 'https://parseapi.back4app.com/';
