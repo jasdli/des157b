c81ad87f985bbcd8dafc900f726107fe3f2b8377
Jasmine Li <jasmineli1215@gmail.com>
2023-06-01 13:43:12 -0700

    usability

--- usability/script.js
+++ usability/script.js
@@ -1,3 +1,8 @@
+(function() {
+  'use strict';
+  alert("Welcome to my food guide. My project is about recommending davis local restaurants to my audience. They can upload food images to the website,and users can view not only their own gallery but also browse through images uploaded by other users. (i am not finished with the gallery feature yet.)Task 1: explore the map, find and interact with the icons. Task 2: upload your images to the website. Task 3: Click the button to read the seal connection");
+})();
+
 Parse.initialize("PUqIfC6upxBMI8ktgDLqsT0r5mAF3ma1q1bdaKkb", "FlP4kl3OqVBnEJ0yteYIH4BFCyPKnYhcRhEdQlJx");
 
 Parse.serverURL = 'https://parseapi.back4app.com/';
@@ -60,42 +65,44 @@ function showUploadedPhoto(photoURL, photoName){
 // This is a good place to write a function that clears out the form.
 
 //map
-const map = L.map('map').setView([38.546719, -121.744339], 13);
+(function () {
+  'use strict';
+const map = L.map('map').setView([38.546719, -121.744339], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 20,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 const coffeeIcon = L.icon({
   iconUrl: 'images/cup.png',
-  iconSize: [48, 35],
+  iconSize: [38, 25],
   iconAnchor: [22, 94],
   shadowAnchor: [4, 62],
   popupAnchor: [-3, -76]
 });
-L.marker([38.544123,-121.7391475], { icon: coffeeIcon }).addTo(map);
-marker.bindPopup("<b>temple coffee</b><br>239 G St, Davis, CA 95616").openPopup();
+L.marker([38.544123,-121.7391475], { icon: coffeeIcon }).addTo(map).bindPopup("<strong>Restaurant:</strong>temple coffee<strong>Address:</strong>239 G St, Davis, CA 95616");
+/*marker.bindPopup("<b>temple coffee</b><br>239 G St, Davis, CA 95616").openPopup();*/
 
 const noodleIcon = L.icon({
   iconUrl: 'images/noodle.png',
-  iconSize: [48, 35],
+  iconSize: [78, 65],
   iconAnchor: [22, 94],
   shadowAnchor: [4, 62],
   popupAnchor: [-3, -76]
 });
-L.marker([38.5464673,-121.7400401], { icon: noodleIcon }).addTo(map);
-marker.bindPopup("<b>lazi cow</b><br>407 G St #4, Davis, CA 95616").openPopup();
+L.marker([38.5464673,-121.7400401], { icon: noodleIcon }).addTo(map).bindPopup("<strong>Restaurant:</strong>lazi cow <strong>Address:</strong>407 G St #4, Davis, CA 95616");
+/*marker.bindPopup("<b>lazi cow</b><br>407 G St #4, Davis, CA 95616").openPopup();*/
 
 
 const sandwichIcon = L.icon({
   iconUrl: 'images/sandwich.png',
-  iconSize: [48, 35],
+  iconSize: [68, 45],
   iconAnchor: [22, 94],
   shadowAnchor: [4, 62],
   popupAnchor: [-3, -76]
 });
-L.marker([38.543598, -121.739962], { icon: sandwichIcon }).addTo(map);
-marker.bindPopup("<b>ike's sandwiches</b><br>212 F St b, Davis, CA 95616").openPopup();
+L.marker([38.543598, -121.739962], { icon: sandwichIcon }).addTo(map).bindPopup("<strong>Restaurant:</strong> ike's sandwiches <strong>Address:</strong>212 F St b, Davis, CA 95616");
+/*marker.bindPopup("<b>ike's sandwiches</b><br>212 F St b, Davis, CA 95616").openPopup();*/
 
 const pizzaIcon = L.icon({
   iconUrl: 'images/pizza.png',
@@ -104,22 +111,7 @@ const pizzaIcon = L.icon({
   shadowAnchor: [4, 62],
   popupAnchor: [-3, -76]
 });
-L.marker([38.5441597, -121.7389437], { icon: pizzaIcon }).addTo(map);
-marker.bindPopup("<b>Woodstock Piza Davis</b><br> 238 G St, Davis, CA 95616").openPopup();
-
-
-/*const marker = L.marker([38.5414, -121.7410]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>My Burma").openPopup();
-
-const popup = L.popup()
-    .setLatLng([38.544123,-121.7391475])
-    .setContent("Temple Coffee Roaster at G street")
-    .openOn(map);
-
-    const circle = L.circle([38.54475754871805,-121.73894408741701], {
-        color: 'greem',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 20
-    }).addTo(map);
-    circle.bindPopup("Woodstock Piza Davis");*/
+L.marker([38.5441597, -121.7389437], { icon: pizzaIcon }).addTo(map).bindPopup("<strong>Restaurant:</strong> Woodstock Piza Davis <strong>Address:</strong>238 G St, Davis, CA 95616");
+/*marker.bindPopup("<b>Woodstock Piza Davis</b><br> 238 G St, Davis, CA 95616").openPopup();*/
+})();
+AOS.init();
